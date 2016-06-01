70bc202fa41c5f1288cd038245699ce61de0992a
m00se333 <moosecunningham@gmail.com>
2016-06-01 14:52:55 -0700

    mobile styling

--- src/public/script/staffListScriptIndex.js
+++ src/public/script/staffListScriptIndex.js
@@ -4,48 +4,27 @@ var isiPad = navigator.userAgent.match(/iPad/i) != null;
 
 if (isiPhone === true) {
 	console.log("iPhone test");
-
 	$("#sList").click(function (){
-
-		
 		$("#mobileStaffList").slideToggle("slow");
-
 	});
-
 	$("#mobileStaffList").click(function (){
 		$("#mobileStaffList").slideToggle("slow")
-
 	});
-
-
 } else if (isiPad === true) {
-	console.log("iPad test")
-
+	console.log("iPad test");
 	$("#sList").click(function (){
-
-		
 		$("#mobileStaffList").slideToggle("slow");
-
 	});
-
 	$("#mobileStaffList").click(function (){
 		$("#mobileStaffList").slideToggle("slow")
-
 	});
-
-
 } else {
-
-	console.log("Other test")
+	console.log("Other test");
 	$("#sList").after($("#mobileStaffList"));
-
 	$("#sList").click(function (){
 		$("#mobileStaffList").slideToggle("slow")
-
 	});
-
 	$("#mobileStaffList").click(function (){
 		$("#mobileStaffList").slideToggle("slow")
-
 	});
 }
