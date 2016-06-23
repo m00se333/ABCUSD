56caa79ceffb3f9a4eebe3c35f0b63d7777fe799
m00se333 <moosecunningham@gmail.com>
2016-06-23 14:18:53 -0700

    port is a variable and added a start up script

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ var express = require("express"),
 	cacEvents = require("./mock/test2.json");
 
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.use("/static", express.static(__dirname + "/public"))
 
@@ -23,6 +24,6 @@ app.get("/parentresources", function(req, res){
 	res.render("parentResources");
 });
 
-app.listen("https://abcusd.herokuapp.com", function(){
-	console.log("Frontend server is running on port 5555.")
+app.listen(port, function(){
+	console.log("Frontend server is running at " + port);
 });
