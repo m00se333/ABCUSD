6fd5511d72c19c51f0a50311f74ecda039d26670
m00se333 <moosecunningham@gmail.com>
2016-06-28 21:33:20 -0700

    ./src/... format in app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 "use strict";
 
 var express = require("express");
-var	cacOfficers = require("src/mock/test.JSON");
-var	cacEvents = require("src/mock/test2.JSON");
+var	cacOfficers = require("./src/mock/test.JSON");
+var	cacEvents = require("./src/mock/test2.JSON");
 
 var app = express();
 var port = process.env.PORT || 8080;
