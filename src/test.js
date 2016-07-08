d615c687d05a949565830349eb1bf0c598523926
m00se333 <moosecunningham@gmail.com>
2016-07-08 12:16:20 -0700

    change test.JSON take out information, change styles for readability

--- src/test.js
+++ src/test.js
@@ -1,40 +1,24 @@
 var members = {
-	"2014-2016 Term": {
-		"dates": "2014-2016",
-		"members": [
-			{
-				"name": "Clara Kim",
-				"number": "(562)-715-9696",
-				"email": "cdbk79@yahoo.com"
-			},
-			{
-				"name" : "Narciso Villegas",
-				"number": "(562)-413-8972",
-				"email": "narciso.villegas96@gmail.com"
-			},
-			{
-				"name" : "Maria Pia Avendano",
-				"number": "(562)-674-722 (C)",
-				"email": "mariapia_ava@hotmail.com"
-			}
-				]
-
-			},
-	"2015-2017 Term": {
-		"dates": "2015-2017",
-		"members": [
-			{
-				"name" : "Katherine Patel",
-				"number": "(714)-402-7186 (C)",
-				"email": "katherinepatel@yahoo.com"
-			},
-			{
-				"name" : "Evelyn Rodriguez",
-				"number": "(562)-348-6480",
-				"email": "epamela31@gmail.com"
-			}
-					]
-			}
-};
+	"2016-2017": {
+		
+		"dates": "2016-2017",
+		
+		"members": 
+			[
+				{
+					"name" : "Katherine Patel, Chairperson"
+				},
+				{
+					"name": "Clara Kim, Vice-Chairperson"
+				},
+				{
+					"name" : "Maria Pia Avendano"
+				},
+				{
+					"name" : "Evelyn Rodriguez"
+				}	
+			]
+		}
+	}
 
 module.exports = members;
