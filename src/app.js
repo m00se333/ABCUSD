a8fea9569257aee389a8d72f478dd6667aef3bee
m00se333 <moosecunningham@gmail.com>
2016-07-12 14:10:27 -0700

    added Firebase and I have some reading and writing capability already implemented on the CAC page

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 "use strict";
 
 var express = require("express");
-var	cacOfficers = require("./test.js");
+//var	cacOfficers = require("./test.js");
 var	cacEvents = require("./test2.js");
+var firebase = require("firebase");
+firebase.initializeApp({
+  databaseURL: "https://abcusdserver.firebaseio.com/",
+  serviceAccount: {
+  "type": "service_account",
+  "project_id": "abcusdserver",
+  "private_key_id": "de201c32c9d8bdc97c8cbfe409a59c0c82b45de9",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCPKvI6BMcZBcOB\n66ssmotfF2lY5F+U4jKEoueXouQHiqnWNg9GQVM5SCzYyll7NQppLGOmMLKpLKYi\nn4FteKpeSADWEvZuAV2qbNGcvpAgqi4jIeIpvGp6AMO6U0J4SKL2yodw5T6uKJ/i\n86cneQpgMlgOHmHjj3L49F/4xIMu1VYrZkqE2lUSJmS00JMMIRrXaVLyADRJciTt\nwSrELe6ZSbxhxiMsSSpp4iVXByYosvMPqe1xw2ID0SvsV7oqUDSFgc9B58GRGTgV\ngODjYiGTaPX8ACWnzjoSJFhskr4wTvByqU4tdaYclZVsEUtFsNFtVVJdypSzwD8T\nx1Y/aGC7AgMBAAECggEAAZjOCN2ixuGsk5doWmAC6/Wb5DTv5/456AbVJHjZqEfg\nqxYBR5xU7E+kyGpBY0FtM00ec20ZoQ9xbvNugyUUaWbOM5UUkJeNTuhV0jTB8MtV\nJmkrV6HVYjT9+ecR17ufrbcGmbjE4TZsyiBDngoYlogZOVcXam1gL+yx4Vl3yR7t\nvu/olZDJPLXmHDNoPU7f+tfCwvA1QZXDyfyqG3YTTU/zIWxFsQCwGJh2hzreugiH\nJR39JCXQ9mwZ4Co/2VhwcCPB0EVAoSkA3CJOPJVGY2chc9lpiUrjIIkWGDuaQmMg\nTdQo8YCb6B8VGpbLJShN1XSucyWU1WgY5LDGDqa3gQKBgQDa8Uczj06CshEo28yO\nbScTYT+3EeWUV5xiUayGqlkDgOVCGNxmtVGBWqvhlsl1oWVZPDPPlzqwMNGPApcL\nHggAsEJWakTruoTRbBgMrnPWuENKCuEHGm0RJmaDdH699Br5OZRqP79bODA/mzuV\nOJjZ07Onr+lvVQvatREHwXOh+wKBgQCnZl5LUuvY2vUT47Ddf2sLaaVeseM5aDJu\n/2FNjmLVmNRjZBaFvIaCcAXkmkeCr3zQ7rsEPagsqaboHTBk8kQTaVD/qWLIYspT\nWjSEv8h68m3dU+1BU3KF7Wr2ehw7xgyAK0LY1xsCJNZfYugD0FnPD3hgJUndO1uv\nY2GlMbDAQQKBgQCBHdNIo9XAG7Uk7GK5B8QImMxTcTl7/yLZlKLzE9jEySnqjfDE\nnV+AsnE065DJTzIOqwM/BEd3CoM7HGLpCn/bsBafEbKtdeTIAEA6klL8KNR+AOZu\nIvZ5K7U/Bt+LtD6IBZ3x/sAh0oQZrrk3qhXyN/f2BzbaEkSYaTmu4jYIKwKBgA5g\nICDf0hkhVIKMvxeGZa7pN6ZWKGk5pXsau0RpwdsN8M9IPRbnBbYV4KutBA8Y+uMG\nXVET6mlLeGXee9VvaKEyHh6WcUE8Gpvl1zS/M3w7bWvgVTOS8vbFWLYp7lrDuzcW\nf/sXHd6lQzOVzs7lCZFahr/a8+jnvAvnwO7qwX8BAoGAfr9sNtbEjCj/Kn5XBj3+\nVQlPGOpyDLeEGEg3Y+e69RpBrxMt4i8VA1COH3pjcmQOc0jxQRK+hZDUcCbGzXNT\nFEsbiejnopRG1+HSMmRebSJBXYpwmAK1J8EXtLXL9itB1XC4Usg20VmRDU5+dTPP\neNIVA3Gif7TuoWC4CnBEGZg=\n-----END PRIVATE KEY-----\n",
+  "client_email": "abcusd@abcusdserver.iam.gserviceaccount.com",
+  "client_id": "110981819402347223212",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://accounts.google.com/o/oauth2/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/abcusd%40abcusdserver.iam.gserviceaccount.com"
+  }
+  
+});
 
 var app = express();
 var port = process.env.PORT || 8080;
@@ -17,7 +34,11 @@ app.get("/", function(req, res){
 });
 
 app.get("/cac", function(req, res){
-	res.render("cacTest", {terms: cacOfficers, events: cacEvents});
+  firebase.database().ref("/CAC").once("value", function(snapshot){
+    console.log(snapshot.val());
+    var members = snapshot.val();
+    res.render("cacTest", {members: members, events: cacEvents});
+  })
 });
 
 app.get("/parentresources", function(req, res){
--- src/public/script/updateDatabase.js
+++ src/public/script/updateDatabase.js
@@ -1,4 +1,4 @@
-var newMembersMockup = {}
+var newMembersMockup = [];
 var nameField = $("#name");
 var phoneField = $("#phone");
 var emailField = $("#email");
@@ -14,18 +14,28 @@ var config = {
   };
 firebase.initializeApp(config);
 
+function clearFields(){
+  $("#updateItem input[type=text], textarea").val("");
+}
 
+// create/update funcitionality
 function captureInput(node, eventHandler){
+      
       node.on(eventHandler, function(event){
           event.preventDefault();
-
           var inputFields = {
             name: nameField.val(),
             phone: phoneField.val(),
             email: emailField.val()
           };
+
+          for (key in inputFields){
+            if (inputFields[key] === ""){
+              delete inputFields[key];
+            }
+          }
           
-          newMembersMockup.push(inputFields);
+          newMembersMockup.push(inputFields)
           var previewTemplate = $("#previewItem").html();
           var template = Handlebars.compile(previewTemplate);
           var previewTemplateHTML = template(inputFields);
@@ -33,18 +43,31 @@ function captureInput(node, eventHandler){
           $("#updatePreview").append($previewTemplateHTML);
           console.log(newMembersMockup);
           //throwing jade handlebars templates.
-
-          firebase.database().ref("/CAC").push(inputFields);
+          clearFields();
         });
 
 }
 
+// Updataing Database
 function pushUpdates(node, eventHandler){
   node.on(eventHandler, function(event){
     event.preventDefault();
     firebase.database().ref("/CAC").set(newMembersMockup)
   })
 }
+
+// Retrieving Data
+$previewButton.on("click", function(event){
+  event.preventDefault();
+  firebase.database().ref("CAC").once("value", function(snapshot){
+    var databaseLength = snapshot.val().length;
+
+    for (var x = 0; x < databaseLength; x++){
+      console.log(snapshot.child(x).val());
+    }
+
+  });
+});
 
 
 
